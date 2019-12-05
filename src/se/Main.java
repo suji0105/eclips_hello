ee2eed448b2c86e0f76d6a64e7c3b2f374d93de7
suji0105 <43868538+suji0105@users.noreply.github.com>
2019-12-05 12:59:02 +0900

    helloworld100

--- src/se/Main.java
+++ src/se/Main.java
@@ -2,7 +2,7 @@ package se;
 
 public class Main {
 	public static void main(String[] args) {
-		System.out.print("Hello world3");
+		System.out.print("Hello world100");
 		System.out.println("1:branch hotfix");
 	}
 }
